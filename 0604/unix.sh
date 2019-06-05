81fca3805af10f0a71ee6ceb0a9be6a4c357303d
ranto <n19006@std.it-college.ac.jp>
2019-06-05 12:38:07 +0900

    1000

---
+++ 0604/unix.sh
@@ -0,0 +1,30 @@
+
+n cont() {
+echo -n 'continue(y/n)'
+read yesno
+if [ $yesno = 'n' ]; then
+    echo 'end...'
+    exit
+fi
+echo 'continue...'
+}
+cd $HOME/repo/unix1
+git status
+cont
+echo 'a.git add-i b.git commit c.git push'
+read a
+case "$a" in
+    a)
+        git add -i
+        git status
+        ;;
+   b)
+        echo -n 'comment:'
+        read comment
+        git commit -m "$commentii"
+        git log --online
+        ;;
+    c)
+        git push
+        ;;
+esac
